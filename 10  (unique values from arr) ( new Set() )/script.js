d63b13a35a7f9cf960fffbcc5072d39475a0f0a6
ShihabPalekar <ShihabPalekar23@gmail.com>
2024-07-25 11:59:56 +0530

    chnages

--- 10  (unique values from arr) ( new Set() )/script.js
+++ 10  (unique values from arr) ( new Set() )/script.js
@@ -1,32 +1,42 @@
-//Without using Set
+//Without using Set - solution 1
 function uniqueValues(arr) {
   const uniqueArr = [];
-  
-  arr.forEach(element => {
+
+  arr.forEach((element) => {
     if (!uniqueArr.includes(element)) {
       uniqueArr.push(element);
     }
   });
-  
+
   return uniqueArr;
 }
+
+
+//Without using Set - solution 2
+const array1 = [1, 2, 3, 3, 4, 5, 6, 4, 2, 1, 5, 8];
+const ans1 = [];
+
+array1.forEach((i) => !ans1.includes(i) && ans1.push(i));
+console.log(ans1);
+
 
 //With using Set
 function uniqueValuesSet(arr) {
   return [...new Set(arr)];
 }
 
+
 //Without using array.includes
 function uniqueValues2(arr) {
   const uniqueArr = [];
   const seen = {};
-  
-  arr.forEach(element => {
+
+  arr.forEach((element) => {
     if (!seen[element]) {
       seen[element] = true;
       uniqueArr.push(element);
     }
   });
-  
+
   return uniqueArr;
 }
